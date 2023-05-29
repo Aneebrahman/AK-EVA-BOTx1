fcfbea91716ed7a1d28e6cad699bcb067d17d319
Rippo123 <98217663+Rippo123@users.noreply.github.com>
2023-05-29 16:41:31 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -68,8 +68,9 @@ async def next_page(bot, query):
             InlineKeyboardButton(
                 text=f" 🎬{get_size(file.file_size)} 🍿 {file.file_name}", callback_data=f'files#{file.file_id}'
             ),
+        ]
         for file in files
-    ]
+]
 else:
     btn = [
         [
