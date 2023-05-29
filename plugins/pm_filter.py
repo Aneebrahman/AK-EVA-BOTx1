9fe4ae9043093f27236b106dfa967daffa135ee9
vpcreatz <119391455+vpcreatz@users.noreply.github.com>
2023-05-29 15:13:36 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -84,7 +84,7 @@ async def next_page(bot, query):
             ]
             for file in files
         ]
-    elif:
+   elif:
         btn = [
             [
                 InlineKeyboardButton(
